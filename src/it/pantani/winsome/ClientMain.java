9dcd4dc0d73ed65de71763eb965264913f3931f2
leonardopantani <leonardo.pantani@gmail.com>
2021-12-02 21:55:10 +0100

    Initial commit

---
+++ src/it/pantani/winsome/ClientMain.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021/2022
+ * Leonardo Pantani - 598896
+ * University of Pisa - Department of Computer Science
+ */
+
+package it.pantani.winsome;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("E tutto ebbe inizio - client");
+    }
+}
